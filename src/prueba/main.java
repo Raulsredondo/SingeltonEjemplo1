bdba20712628522db42b3903fcf0fb9d7e213de1
Raulsredondo <raulsredondo2@gmail.com>
2021-12-21 09:10:40 +0100

    Con el main

--- src/prueba/main.java
+++ src/prueba/main.java
@@ -4,6 +4,9 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		persona p1 = persona.Instancia(18, "Raul");
+		
+		System.out.println(p1);
 
 	}
 
